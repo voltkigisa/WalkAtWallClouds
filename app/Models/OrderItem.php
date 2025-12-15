7ff229156c79825f68eccbbf3855a4cd1dc9da8f
voltkigisa <raden.233040043@mail.unpas.ac.id>
2025-12-16 01:13:27 +0700

    feat(models): Membuat 8 tabel dengan relasinya

---
+++ app/Models/OrderItem.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class OrderItem extends Model
+{
+    /** @use HasFactory<\Database\Factories\OrderItemFactory> */
+    use HasFactory;
+
+    protected $fillable = [
+        'order_id',
+        'ticket_type_id',
+        'quantity',
+        'price',
+    ];
+
+    public function order()
+    {
+        return $this->belongsTo(Order::class);
+    }
+
+    public function ticketType()
+    {
+        return $this->belongsTo(TicketType::class);
+    }
+    public function tickets()
+    {
+        return $this->hasMany(Ticket::class);
+    }
+}
--- app/Models/User.php
+++ app/Models/User.php
@@ -11,34 +11,20 @@ class User extends Authenticatable
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory, Notifiable;
-
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var list<string>
-     */
     protected $fillable = [
         'name',
         'email',
         'password',
+        'role',
+        'phone',
     ];
 
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var list<string>
-     */
     protected $hidden = [
         'password',
         'remember_token',
     ];
 
-    /**
-     * Get the attributes that should be cast.
-     *
-     * @return array<string, string>
-     */
-    protected function casts(): array
+    public function orders()
     {
         return [
             'email_verified_at' => 'datetime',
