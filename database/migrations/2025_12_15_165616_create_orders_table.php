bb92b3c372056e091215ef206e475cd4b893e1ae
voltkigisa <raden.233040043@mail.unpas.ac.id>
2025-12-16 01:14:57 +0700

    [feat]:database: Membuat blueprint untuk databese

---
+++ database/migrations/2025_12_15_165616_create_orders_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()->cascadeOnDelete();
+            $table->string('order_code')->unique();
+            $table->integer('total_price');
+            $table->enum('status', ['pending','paid','cancelled','expired'])->default('pending');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('orders');
+    }
+};
