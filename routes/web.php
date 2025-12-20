d86891785ae772829d63aaec4985caa9b9a2a865
Muhammad Haekal Al ghazy <m.haekalalghazy078@gmail.com>
2025-12-20 18:38:25 +0700

    fix/conflict-routes

--- routes/web.php
+++ routes/web.php
@@ -1,13 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Auth;
 use App\Http\Controllers\LoginController;
 use App\Http\Controllers\RegisterController;
 use App\Models\User;
 use Illuminate\Support\Facades\Hash;
-use Illuminate\Support\Facades\Auth;
 
 Route::get('/', function () {
     return view('welcome');
