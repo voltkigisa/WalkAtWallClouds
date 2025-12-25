c108d4fb8e97d4e2a7c5e1b97eff7f858b53d02b
RamaRizki23 <rama23rizki04@gmail.com>
2025-12-25 18:38:32 +0700

    feat: implement Artist CRUD with HTML views and validation

---
+++ resources/views/artists/create.blade.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Tambah Artist</title>
+</head>
+<body>
+    <h1>Tambah Artist Baru</h1>
+    
+    @if($errors->any())
+        <div>
+            <strong>Error:</strong>
+            <ul>
+                @foreach($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        </div>
+    @endif
+    
+    <form action="{{ route('artists.store') }}" method="POST">
+        @csrf
+        
+        <table>
+            <tr>
+                <td><label for="name">Name:</label></td>
+                <td><input type="text" id="name" name="name" value="{{ old('name') }}" required></td>
+            </tr>
+            <tr>
+                <td><label for="country">Country:</label></td>
+                <td><input type="text" id="country" name="country" value="{{ old('country') }}" required></td>
+            </tr>
+            <tr>
+                <td><label for="genre">Genre:</label></td>
+                <td><input type="text" id="genre" name="genre" value="{{ old('genre') }}" required></td>
+            </tr>
+            <tr>
+                <td><label for="photo">Photo URL:</label></td>
+                <td><input type="text" id="photo" name="photo" value="{{ old('photo') }}"></td>
+            </tr>
+            <tr>
+                <td colspan="2">
+                    <button type="submit">Simpan Artist</button>
+                    <a href="{{ route('artists.index') }}">Batal</a>
+                </td>
+            </tr>
+        </table>
+    </form>
+</body>
+</html>
