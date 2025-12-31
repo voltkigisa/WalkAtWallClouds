37ceb43b2ea3335544a284e3af397b13a6410238
voltkigisa <raden.233040043@mail.unpas.ac.id>
2025-12-31 21:14:44 +0700

    merapihkan fil dan folder

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -1,6 +1,7 @@
 <?php
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\Auth;
 
+use App\Http\Controllers\Controller;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
